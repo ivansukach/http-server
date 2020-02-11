25be5e24209e7f0823e3acb5e41816a8b539f1bf
ivansukach <vsukach1999@gmail.com>
2020-02-11 10:27:31 +0300

    Bugs in signUp.go have been fixed

--- handlers/signUp.go
+++ handlers/signUp.go
@@ -4,20 +4,25 @@ import (
 	"context"
 	"github.com/labstack/echo"
 	"github.com/leshachaplin/grpc-server/protocol"
+	"log"
 	"net/http"
 )
 
 func (a *Auth) SignUp(c echo.Context) error {
+	log.Println("Метод signUp запущен")
 	user := new(UserModel)
 	if err := c.Bind(user); err != nil {
 		return err
 	}
-	requestRegistration := &protocol.SignInRequest{Login: user.Login, Password: user.Password}
-	responseRegistration, err := a.client.SignIn(context.Background(), requestRegistration)
+	requestRegistration := &protocol.SignUpRequest{Login: user.Login, Password: user.Password}
+	log.Println("Сформирован запрос")
+	responseRegistration, err := a.client.SignUp(context.Background(), requestRegistration)
+	log.Println("Получен ответ")
 	if err != nil {
 		return err
 	}
+	log.Println("Получение токена")
 	tokenString := responseRegistration.GetToken()
-
+	log.Println("Метод signUp отработал без ошибок")
 	return c.JSON(http.StatusOK, &TokenModel{Token: tokenString})
 }
